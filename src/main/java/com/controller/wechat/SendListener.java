38bcd0027b1bfa1bde38a8206f40645e4b08c0ff
Jungieve <zihan.cong@gmail.com>
2018-02-07 01:29:34 +0800

    微信移动端

---
+++ src/main/java/com/controller/wechat/SendListener.java
@@ -0,0 +1,88 @@
+package com.controller.wechat;
+
+
+import com.listener.SendTask;
+import com.util.PropertyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+
+/**
+ * @author phan at 2016年3月2日
+ */
+@Component
+public class SendListener implements ApplicationListener<ContextRefreshedEvent> {
+    //	private long DAY = 24*60*60*1000;
+    private long DAY = 60 * 1000;
+    private Timer timer;
+    private int sendHour;
+    private int sendMin;//=Integer.parseInt(PropertyUtil.getProperty("sendMin"));
+    @Autowired
+    private SendTask sendTask;
+    //private ServletContext context;
+
+
+    public void resetSendTime(int hour, int min) {
+        this.sendHour = hour;
+        this.sendMin = min;
+        this.timer.cancel();
+        Calendar calendar = Calendar.getInstance();
+        int curHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int curMin = calendar.get(Calendar.MINUTE);
+        long delay = 0;
+        if (curHour > sendHour && curMin > sendMin) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        calendar.set(Calendar.HOUR_OF_DAY, this.sendHour);
+        calendar.set(Calendar.MINUTE, this.sendMin);
+        Date execPoint = calendar.getTime();//确切的推送事件点
+        delay = execPoint.getTime() - new Date().getTime();
+
+        //定义定时器
+        timer = new Timer("推送", true);
+
+        //启动推送任务,每天执行一次
+        timer.schedule(this.sendTask, delay, DAY);
+
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (event.getApplicationContext().getParent() != null)
+            return;//防止子容器多次调用
+        this.sendHour = Integer.parseInt(PropertyUtil.getProperty("sendHour"));
+        this.sendMin = Integer.parseInt(PropertyUtil.getProperty("sendMin"));
+        Calendar calendar = Calendar.getInstance();
+        int curHour = calendar.get(Calendar.HOUR_OF_DAY);
+        int curMin = calendar.get(Calendar.MINUTE);
+        long delay = 0;
+        if (curHour > sendHour || (curHour == sendHour && curMin > sendMin)) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        calendar.set(Calendar.HOUR_OF_DAY, this.sendHour);
+        calendar.set(Calendar.MINUTE, this.sendMin);
+        Date execPoint = calendar.getTime();//确切的推送事件点
+        delay = execPoint.getTime() - new Date().getTime();
+
+        //定义定时器
+        timer = new Timer("推送", true);
+
+        //启动推送任务,每天执行一次
+        timer.schedule(this.sendTask, delay, DAY);
+//		timer.schedule(this.sendTask,0, DAY );
+        //this.context.setAttribute("sendListener", this);
+
+    }
+
+//	@Override
+//	public void setServletContext(ServletContext servletContext) {
+//		this.context = servletContext;
+//
+//	}
+
+}
--- src/main/java/com/util/AccessTokenUtil.java
+++ src/main/java/com/util/AccessTokenUtil.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Component;
 
 /**
  * @author phan at 2016年2月15日
- *
  */
 @Component
 public class AccessTokenUtil {
@@ -35,19 +34,15 @@ public class AccessTokenUtil {
 	private long currentTime = 0;
 	private long freshTime = 1000 * 60 * 60;
 
-	public AccessToken getAccessToken()
-	{
+	public AccessToken getAccessToken() {
 		/*
 		 * 增加java锁，在accesstoken过期后，
 		 * 假如同时收到好几个请求，这时可以保证请求的正常执行顺序
 		 */
-		synchronized(this)
-		{
-			if((System.currentTimeMillis() - currentTime) > freshTime)
-			{
+		synchronized (this) {
+			if ((System.currentTimeMillis() - currentTime) > freshTime) {
 				accessToken = getAccessToken(appId, appSecret);
-				if(!accessToken.getToken().equals(null) && !accessToken.getToken().equals(""))
-				{//如果accessToken获取成功
+				if (!accessToken.getToken().equals(null) && !accessToken.getToken().equals("")) {//如果accessToken获取成功
 					System.out.println("accessToken获取成功：" + accessToken.getToken());
 					currentTime = System.currentTimeMillis();
 				}
@@ -57,8 +52,7 @@ public class AccessTokenUtil {
 		}
 	}
 
-	public AccessToken getAccessToken(String appId, String appSecret)
-	{
+	public AccessToken getAccessToken(String appId, String appSecret) {
 		AccessToken accessToken = new AccessToken();
 		//访问微信服务器端
 		String url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
@@ -68,8 +62,7 @@ public class AccessTokenUtil {
 		System.out.println("构造url后，accessToken：" + accessTokenString);
 		JSONObject json = JSONObject.fromObject(accessTokenString);
 
-		if (json.containsKey("NULL"))
-		{
+		if (json.containsKey("NULL")) {
 			return null;
 		}
 
@@ -80,25 +73,26 @@ public class AccessTokenUtil {
 	}
 
 	// 获取访问权限码URL
-    private final static String ACCESS_TOKEN_URL = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
-    
-	public static String getAccessToken(HttpServletRequest request){
-		if(request==null){
+	private final static String ACCESS_TOKEN_URL = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+
+	public static String getAccessToken(HttpServletRequest request) {
+		if (request == null) {
 			return refleshAccessToken(null);
 		}
-		AccessToken accesstoken =null;
+		AccessToken accesstoken = null;
 		ServletContext context = request.getSession().getServletContext();
 		accesstoken = (AccessToken) context.getAttribute("accesstoken");
-		if(accesstoken!=null){
+		if (accesstoken != null) {
 			Date createTime = accesstoken.getCreateTime();
-			if((new Date().getTime()-createTime.getTime())<7200*1000){
-				System.out.println("accesstoken:"+accesstoken.getAccessToken());
+			if ((new Date().getTime() - createTime.getTime()) < 7200 * 1000) {
+				System.out.println("accesstoken:" + accesstoken.getAccessToken());
 				return accesstoken.getAccessToken();
 			}
 		}
 		return refleshAccessToken(request);
 
 	}
+
 	private static String refleshAccessToken(HttpServletRequest request) {
 		String result = null;
 		HttpClient client = HttpClients.createDefault();
@@ -110,25 +104,29 @@ public class AccessTokenUtil {
 				HttpEntity entity = response.getEntity();
 				if (entity != null) {
 					BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
-					Boolean line = null;
-					while ((line = (br.readLine()) != null)) {
-						try {
-							JSONObject json = new JSONObject(line);
-							result = json.getString("access_token");
-							if (request != null) {
-								ServletContext context = request.getSession().getServletContext();
-								AccessToken at = new AccessToken();
-								at.setAccessToken(result);
-								at.setCreateTime(new Date());
-								context.setAttribute("accesstoken", at);
-							}
-
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+					String line = null;
+					StringBuffer res = new StringBuffer();
+					while ((line = br.readLine()) != null) {
+						res.append(line);
+					}
+					System.out.println(res.toString());
+					try {
+						JSONObject json = JSONObject.fromObject(res.toString());
+						result = json.getString("access_token");
+						if (request != null) {
+							ServletContext context = request.getSession().getServletContext();
+							AccessToken at = new AccessToken();
+							at.setAccessToken(result);
+							at.setCreateTime(new Date());
+							context.setAttribute("accesstoken", at);
 						}
+
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
 				}
+
 			}
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
